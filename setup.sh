d0ec3439e4b010973892aab26c0ac9495efa8f1d
lars010101 <155564647+lars010101@users.noreply.github.com>
2024-02-05 17:53:20 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,21 @@
 #!/bin/sh
-apk update
-apk upgrade
-apk add bind-tools
 
 # Install cloudflared
+apk add bind-tools
 wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-lin
 ux-arm
 mv -f ./cloudflared-linux-arm /usr/local/bin/cloudflared
 mv ./init.d/cloudflared /etc/init.d/cloudflared
+mv ./conf.d/cloudflared /etc/conf.d/cloudflared
 chmod +x /usr/local/bin/cloudflared
 chmod a+x /etc/init.d/cloudflared
 adduser -s /usr/sbin/nologin -S -H cloudflared
+rc-update add cloudflared default
+
 # Verify installation
 cloudflared -v
-/etc/init.d/cloudflared start
+rc-service cloudflared start
 dig @127.0.0.1 -p 5053 google.com
 
-# Install pihole
-vi /etc/apk/repositories   # enable the edge community repository
-apk update
-apk add bash
-git clone https://gitlab.com/yvelon/pi-hole
-cd pi-hole
-bash automated\ install/basic-install.sh
+# Prepare to make persistent
+lbu add /usr/local/bin/cloudflared /etc/init.d/cloudflared /etc/conf.d/cloudflared
